654d26317eef6fa15f2f71db713589ac1a4786b0
maxime-morfin-dev <maxime.morfin-dev@gmail.com>
2024-04-07 19:59:52 +0200

    🚧 try to add flag to commit and clip it

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,21 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('commit-naming-convention.formatCommitMessage', () => {
+		vscode.window.showInputBox({ prompt: 'Entrez votre message de commit' }).then(message => {
+				if (message) {
+						const formattedMessage = formatCommitMessage(message);
+						vscode.env.clipboard.writeText(formattedMessage); // Copie le message formaté dans le presse-papiers
+						vscode.window.showInformationMessage('Message de commit formaté copié dans le presse-papiers.');
+				}
+		});
+});
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commit-naming-convention" is now active!');
+function formatCommitMessage(message: string): string {
+	return `feat : ${message}`;
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('commit-naming-convention.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from commit-naming-convention!');
-	});
-
-	context.subscriptions.push(disposable);
+context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
